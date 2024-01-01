92e8746bf450828de4c02cb3e3e0a839c1d7e506
Sycada <pantho0>
2024-01-01 20:23:50 +0600

    loading spinner and conditional room card showing functionalities added

--- src/components/Rooms/Rooms.jsx
+++ src/components/Rooms/Rooms.jsx
@@ -1,21 +1,45 @@
 import { useEffect, useState } from "react";
 import Cards from "./Cards";
 import Container from "../Shared/Container";
+import { useSearchParams } from "react-router-dom";
+import Heading from "../Shared/Heading";
+import Loader from "../Shared/Loader";
 
 const Rooms = () => {
   const [rooms, setRooms] = useState([]);
+  const [params, setParams] = useSearchParams();
+  const [loading, setLoading] = useState(false)
+  const category = params.get("category");
   useEffect(() => {
+    setLoading(true)
     fetch("./rooms.json")
       .then((res) => res.json())
-      .then((data) => setRooms(data));
-  }, []);
+      .then((data) => {
+        if (category) {
+          const filtered = data.filter((room) => room.category === category);
+          setRooms(filtered);
+        } else setRooms(data);
+        setLoading(false)
+      });
+  }, [category]);
+  if(loading) return <Loader/>
   return (
     <Container>
-      <div className="grid grid-cols-1 pt-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8">
-        {rooms.map((room) => (
-          <Cards key={room._id} room={room} />
-        ))}
-      </div>
+      {rooms && rooms.length > 0 ? (
+        <div className="grid grid-cols-1 pt-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8">
+          {rooms.map((room) => (
+            <Cards key={room._id} room={room} />
+          ))}
+        </div>
+      ) : (
+        <div className="flex justify-center items-center min-h-[calc(100vh-300px)]">
+          <Heading
+            center={true}
+            title={"No Rooms Available In This Category"}
+            subtitle={"Please Browse Another Category"}
+          />
+        </div>
+      )}
     </Container>
   );
 };
