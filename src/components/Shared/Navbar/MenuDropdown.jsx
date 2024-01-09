67366eebd02c9a0f68986bfdd9972d7df5020ae9
pantho0 <pantho0>
2024-01-09 21:20:01 +0600

    signup link updated with link

--- src/components/Shared/Navbar/MenuDropdown.jsx
+++ src/components/Shared/Navbar/MenuDropdown.jsx
@@ -69,7 +69,7 @@ const MenuDropdown = () => {
               Login
             </Link>
             <Link
-              to='/signu'
+              to='/signup'
               className='px-4 py-3 hover:bg-neutral-100 transition font-semibold'
             >
               Sign Up
